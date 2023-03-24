d2f90157e4f63b86e8a0cec69141669657a48ec8
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-03-24 16:23:26 -0300

    fix: refatoração total do master-install.sh

--- master-install.sh
+++ master-install.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-
-set -e
-
-# Função para instalar todos os programas disponíveis no repositório
-install_all() {
-  sudo pacman -Syu --noconfirm
-  yay -Syu --noconfirm
-  yay -S --noconfirm - < <(yay -Qeq | grep -v "$(pacman -Qmq)" | grep -v "$(pacman -Qgq base)" | sed 's|^.*\/||')
-}
-
-# Função para instalar manualmente os programas desejados
-install_manual() {
-  echo "Digite o nome dos programas que deseja instalar (separados por espaço): "
-  read -r -a packages
-  yay -S --noconfirm "${packages[@]}"
-}
-
 echo "Executando script master..."
+echo "Verificando se o sistema está atualizado..."
+if ! sudo pacman -Syup &>/dev/null; then
+  echo "O sistema não está atualizado, atualizando agora..."
+  sudo pacman -Syu --noconfirm -y
+fi
 
-# Verifica se o sistema está atualizado
-sudo pacman -Syu --noconfirm
+# Função para instalar programas manualmente
+function install_manual() {
+  echo "Digite o nome dos programas que deseja instalar separados por espaço:"
+  read programs
+  for program in $programs; do
+    echo "Instalando $program..."
+    sudo pacman -S --noconfirm $program
+  done
+}
 
-# Verifica se o Yay está instalado e, caso não esteja, realiza a instalação
+# Instalação do Yay
 if ! command -v yay &>/dev/null; then
   echo "Instalando o Yay..."
+  sudo pacman -Syu --noconfirm git base-devel
   git clone https://aur.archlinux.org/yay.git
   cd yay
   makepkg -si --noconfirm
@@ -33,28 +29,47 @@ else
   echo "Yay já está instalado. Ignorando a instalação."
 fi
 
-# Menu de opções
-while true; do
+# Função para instalar todos os programas do repositório
+function install_all() {
+  # Mensagem para informar que todos os programas do repositório serão instalados
+  echo "Instalando todos os programas do repositório..."
+
+  # URL do repositório onde os arquivos .sh estão hospedados
+  repo_url="https://raw.githubusercontent.com/lucas-de-lima/manjaro-installation-scripts/main/programs/"
+
+  # Loop para percorrer todos os arquivos .sh no repositório
+  for file in $(curl -sSL $repo_url); do
+    # Mensagem para informar qual programa está sendo instalado
+    echo "Instalando $file..."
+
+    # Comando para baixar e executar o arquivo .sh correspondente ao programa
+    curl -sSL $repo_url$file | bash
+  done
+}
+
+# Verifica se o usuário deseja instalar programas manualmente ou todos do repositório
+if [[ $1 == "manual" ]]; then
+  install_manual
+else
+  # Prompt para o usuário escolher entre instalar tudo ou escolher manualmente
   echo "Bem-vindo ao script de instalação do Manjaro! Escolha uma das opções abaixo:"
   echo "1 - Instalar todos os programas disponíveis no repositório"
   echo "2 - Instalar manualmente os programas desejados"
-  read -p "Opção desejada: " option
 
-  case $option in
+  read -r -d '' _ </dev/tty
+  read -p "Opção desejada: " && case $REPLY in
   1)
     # Script para instalar todos os programas do repositório
     install_all
-    break
     ;;
   2)
     # Script para instalar manualmente os programas desejados
     install_manual
-    break
     ;;
   *)
     echo "Opção inválida"
+    exit 1
     ;;
   esac
-done
 
-echo "Script de instalação finalizado!"
+fi
