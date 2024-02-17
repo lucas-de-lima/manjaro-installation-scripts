1cd1b8a91c6cae4b62a2c63f2376c41db0222c30
Lucas De Lima <lucasramosdelima@gmail.com>
2024-02-17 11:13:52 -0300

    fix: chrome instal

--- programs/google-chrome-install.sh
+++ programs/google-chrome-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if yay -Q google-chrome-stable >/dev/null 2>&1; then
+if yay -Q google-chrome >/dev/null 2>&1; then
   echo "Google Chrome já está instalado. Ignorando a instalação."
 else
 
   echo "Instalando o Google Chrome..."
-  yay -S --noconfirm google-chrome-stable
+  yay -S --noconfirm google-chrome
 
 fi
