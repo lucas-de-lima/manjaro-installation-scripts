665200ebdced5ba17f66e1b64a11215e32012a85
Lucas De Lima <lucasramosdelima@gmail.com>
2024-08-23 20:46:24 -0300

    Update visual-studio-code-install.sh

--- programs/visual-studio-code-install.sh
+++ programs/visual-studio-code-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if yay -Q programa >/dev/null 2>&1; then
+# Verifica se o Visual Studio Code (bin) já está instalado
+if pacman -Qs visual-studio-code-bin >/dev/null 2>&1; then
     echo "Visual Studio Code já está instalado. Ignorando a instalação."
 else
     echo "Instalando o Visual Studio Code..."
-    yay -S --noconfirm code
+    yay -S --noconfirm visual-studio-code-bin
 fi
